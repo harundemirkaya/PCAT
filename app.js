da2c9f962d872d958829fcf3e8dd30a8d3fc1a92
Harun Demirkaya <hdk@Harun-MacBook-Air.local>
2022-12-02 12:42:09 +0300

    Static Files Uploaded

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-const fn = function () {
-  console.log('merhaba');
-};
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static('public'));
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './temp/index.html'));
+});
+const port = 3000;
+app.listen(port, () => {
+  console.log('Başlatıldı');
+});
